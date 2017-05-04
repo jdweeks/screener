4a22d122355d5ef134841e8d91df35cf6b5608bd
JD Weeks <jdweeks@g.clemson.edu>
2017-05-04 07:39:15 -0400

    close and volume

--- screener.py
+++ screener.py
@@ -1,39 +1,37 @@
 #!/usr/local/bin/python3
 
 import os
-import quandl
+import quandl as ql
+import pandas as pd
 
 def readRuss():
     try:
         ticks = [] 
-        russ = open('russ3.csv', 'r').read()
+        russ  = open('russ3.csv', 'r').read()
         split = russ.split('\n')
 
         for tick in split:
             ticks.append('WIKI/' + tick.rstrip())
         return ticks
+
     except Exception as e:
         print('Failed to read Russell 3000:', str(e))
 
-def getData(ticks):
+def getData(ticks, start):
     try: 
-        return quandl.get(ticks, start_date='2017-05-01')
+        return ql.get(ticks, start_date = start)
+
     except Exception as e:
         print('Failed to get stock data:', str(e))
 
-def printClose(ticks, data):
-    for tick in ticks:
-        try:
-            print(data[tick+' - '+'Close'])
-        except KeyError:
-            pass 
-
 def main():
-    quandl.ApiConfig.api_key = os.environ['QUANDL_KEY']
+    ql.ApiConfig.api_key = os.environ['QUANDL_KEY']
 
     ticks = readRuss()
-    data = getData(ticks[:20])
-    printClose(ticks[:20], data)
+    close = [ tick + '.4' for tick in ticks[:50] ] 
+    vol   = [ tick + '.5' for tick in ticks[:50] ]
+
+    print(getData(close + vol, '2017-01-01'))
 
 if __name__ == "__main__":
     # execute only if run as a script
