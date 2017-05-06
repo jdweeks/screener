3fd326a41faa3070a455092ec599a508ec4f7860
JD Weeks <jdweeks@g.clemson.edu>
2017-05-06 17:04:55 -0400

    arg parsing

--- screener.py
+++ screener.py
@@ -1,8 +1,9 @@
 #!/usr/local/bin/python3
 
-import os
+import os, sys, getopt
 import quandl as ql
 import pandas as pd
+import numpy  as np
 
 def readRuss():
     try:
@@ -17,22 +18,52 @@ def readRuss():
     except Exception as e:
         print('Failed to read Russell 3000:', str(e))
 
-def getData(ticks, start):
+def getData(ticks, date):
     try: 
-        return ql.get(ticks, start_date = start)
+        return ql.get(ticks, start_date = date)
 
     except Exception as e:
         print('Failed to get stock data:', str(e))
 
-def main():
+def main(argv):
+    tick = 'WIKI/'
+    date = '2017/01/01'
+
     ql.ApiConfig.api_key = os.environ['QUANDL_KEY']
+    usage = 'usage: screener.py -t <ticker> -d <start_date>'
 
-    ticks = readRuss()
-    close = [ tick + '.4' for tick in ticks[:50] ] 
-    vol   = [ tick + '.5' for tick in ticks[:50] ]
+    if len(argv) == 0:
+        print(usage)
+        sys.exit(2)
 
-    print(getData(close + vol, '2017-01-01'))
+    try:
+        opts, args = getopt.getopt(argv, 'ht:d', ['ticker=', 'date='])
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(usage)
+            sys.exit(0)        
+        elif opt in ('-t', '--ticker'):
+            tick += arg
+        elif opt in ('-d', '--date'):
+            date = arg
+
+    close = getData(tick + '.4', date)
+    vol   = getData(tick + '.5', date)
+    print(pd.concat([close, vol], axis=1))
+
+#   ticks = readRuss()
+#   q_close = [ tick + '.4' for tick in ticks[:5] ] 
+#   q_vol   = [ tick + '.5' for tick in ticks[:5] ]
+#   data = getData(q_close + q_vol, '2017-01-01')
+
+#   vol  = selectVol(data);
+#   close = selectClose(data);
+
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main()
+    main(sys.argv[1:])
